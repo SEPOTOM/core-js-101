eac55ff501da0e7bfd287babfdc240574f9c04f8
Artyom Pizin <artempetr2016@gmail.com>
2023-06-16 23:08:27 +0300

    feat: implement sortDigitNamesByNumericOrder function

--- src/03-arrays-tasks.js
+++ src/03-arrays-tasks.js
@@ -333,8 +333,10 @@ function getPositivesCount(arr) {
  *   [ 'nine','eight','nine','eight'] => [ 'eight','eight','nine','nine']
  *   [ 'one','one','one','zero' ]     => [ 'zero','one','one','one' ]
  */
-function sortDigitNamesByNumericOrder(/* arr */) {
-  throw new Error('Not implemented');
+function sortDigitNamesByNumericOrder(arr) {
+  const DIGIT_NAMES = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
+  const findIndex = (item) => DIGIT_NAMES.findIndex((innerItem) => item === innerItem);
+  return arr.sort((a, b) => findIndex(a) - findIndex(b));
 }
 
 /**
