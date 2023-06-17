43089699ea54804f119d394e262462965a44b989
Artyom Pizin <artempetr2016@gmail.com>
2023-06-17 17:10:26 +0300

    feat: implement findAllOccurrences function

--- src/03-arrays-tasks.js
+++ src/03-arrays-tasks.js
@@ -390,8 +390,8 @@ function getFalsyValuesCount(arr) {
  *    [ null, undefined, null ], null => 2
  *    [ true, 0, 1, 'true' ], true => 1
  */
-function findAllOccurrences(/* arr, item */) {
-  throw new Error('Not implemented');
+function findAllOccurrences(arr, item) {
+  return arr.reduce((prev, innerItem) => (innerItem === item ? prev + 1 : prev), 0);
 }
 
 /**
